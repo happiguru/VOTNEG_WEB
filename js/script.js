c625e3e180f80372c47c8015c3e080e0f7fe27a3
happiguru <hguruman@gmail.com>
2020-04-21 22:25:12 +0400

    style the forms today

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ function myFunction() {
   }
 }
 
-
+$(document).ready(function() {
+  $("#news-slider").owlCarousel({
+      items : 3,
+      itemsDesktop:[1199,2],
+      itemsDesktopSmall:[980,2],
+      itemsMobile: [600, 1],
+      navigation : false,
+      pagination:false,
+      autoPlay : true
+  });
+});
