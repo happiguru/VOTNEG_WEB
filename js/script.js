9f42654d17e69873d14bcc81e6465fe66fd7e0b1
happiguru <hguruman@gmail.com>
2020-04-24 15:04:03 +0400

    Made some changes on the head and also added css for logo on the header

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ $(document).ready(function() {
       autoPlay : true
   });
 });
+
+// Contact Form Here
+	var form = document.forms["testimony"];
+	form.addEventListener('submit',testimony_submit,false);
+
+	function testimony_submit(e) {
+		// Stop Form From Submitting
+		e.preventDefault();
+
+		// Set Initial Variables
+		var target = e.target || e.srcElement;
+		var to = 'hguruman@gmail.com';
+		var uri = 'mailto:' + to;
+		var body = '';
+
+		// Set Form Values to Variables
+		var name = target.elements['name'].value;
+		var subject = "Testimony";
+		var email = target.elements['email'].value;
+		var testimony = target.elements['testimony'].value;
+
+		// Build Body / Message with all Input Fields
+		body += testimony + "\r\n\r\n";
+		body += "Name: " + name + "\r\n";
+		body += "Email: " + email + "\r\n";
+
+		// Build final Mailto URI
+		uri += '?subject=' + encodeURIComponent(subject);
+		uri += '&body=' + encodeURIComponent(body);
+
+    let a = document.getElementById('success');
+    
+		// Open Mailto in New Window / Tab
+		window.open(uri,'_blank');
+	}
